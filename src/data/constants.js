12de627e530a7cd23b42b62d372d46f69ae19f9d
BanulaKumarage <banula.19@cse.mrt.ac.lk>
2023-09-05 15:23:14 +0530

    Initial Commit

---
+++ src/data/constants.js
@@ -0,0 +1,11 @@
+const json = require('./Details.json');
+
+export const Bio = json.Bio;
+
+export const skills = json.Skills;
+
+export const experiences = json.Experiences;
+
+export const education = json.Education;
+
+export const projects = json.Projects;
